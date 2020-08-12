3a9fb58f4badee17c0f401635c5e1f44f4cffa28
Lucas-Bispo <lukaobispo@gmail.com>
2020-08-12 10:20:08 -0300

    parte 3 displaying-data

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,29 @@
 import 'package:angular/angular.dart';
-//teste de comite
+
+import 'src/hero.dart';
+
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '''
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is: {{myHero.name}}</h2>
+    <p>Heroes:</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{ hero.name }}
+      </li>
+    </ul>
+    <p *ngIf="heroes.length > 3">There are many heroes!</p>
+  ''',
+  directives: [coreDirectives],
 )
 class AppComponent {
-  var name = 'Angular';
+  final title = 'Tour of Heroes';
+  List<Hero> heroes = [
+    Hero(1, 'Windstorm'),
+    Hero(13, 'Bombasto'),
+    Hero(15, 'Magneta'),
+    Hero(20, 'Tornado')
+  ];
+  Hero get myHero => heroes.first;
 }
