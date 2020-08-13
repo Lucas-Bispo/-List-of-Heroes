b253a70a7d7cfcc80b485c8b5f10da4e971915a7
Lucas-Bispo <lukaobispo@gmail.com>
2020-08-13 11:02:23 -0300

    parte de entrada e saida de dados

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,29 +1,14 @@
 import 'package:angular/angular.dart';
 
-import 'src/hero.dart';
 
-@Component(//meta dados 
+import 'src/little_tour_component.dart';
+
+@Component(
   selector: 'my-app',
-  template: '''
-    <h1>{{title}}</h1>
-    <h2>My favorite hero is: {{myHero.name}}</h2>
-    <p>Heroes:</p>
-    <ul>
-      <li *ngFor="let hero of heroes">
-        {{ hero.name }}
-      </li>
-    </ul>
-    <p *ngIf="heroes.length > 3">There are many heroes!</p>
-  ''',
-  directives: [coreDirectives],
+  templateUrl: 'app_component.html',
+  directives: [
+
+    LittleTourComponent,
+  ],
 )
-class AppComponent {
-  final title = 'Tour of Heroes';
-  List<Hero> heroes = [
-    Hero(1, 'Windstorm'),
-    Hero(13, 'Bombasto'),
-    Hero(15, 'Magneta'),
-    Hero(20, 'Lucas')
-  ];
-  Hero get myHero => heroes.first;
-}
+class AppComponent {}
