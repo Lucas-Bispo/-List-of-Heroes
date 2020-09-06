93415072039f463fadeff14fed726a304dc5671e
Lucas-Bispo <lukaobispo@gmail.com>
2020-09-06 09:26:55 -0300

    parte 5 criando rotas

--- lib/src/hero_service.dart
+++ lib/src/hero_service.dart
@@ -5,8 +5,7 @@ import 'mock_heroes.dart';
 
 class HeroService {
   Future<List<Hero>> getAll() async => mockHeroes;
-  // See the "Take it slow" appendix
-  Future<List<Hero>> getAllSlowly() {
-    return Future.delayed(Duration(seconds: 2), getAll);
-  }
+
+  Future<Hero> get(int id) async =>
+      (await getAll()).firstWhere((hero) => hero.id == id);
 }
